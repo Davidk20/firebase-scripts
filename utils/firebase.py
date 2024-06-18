c11dcd15f3e0c5ebbd94ada6ac6d9b6dd6210a89
David <davidkidd.dev@gmail.com>
2024-06-18 01:25:30 +0100

    added csv support and ci workflow

--- utils/firebase.py
+++ utils/firebase.py
@@ -7,8 +7,21 @@ from utils.terminal import TColors
 
 
 class FirebaseService:
+    """Service managing Firebase Access"""
 
-    def __init__(self, emulator: bool, service_key_path: str) -> None:
+    def __init__(
+        self, emulator: bool, service_key_path: str = "service_key.json"
+    ) -> None:
+        """
+        Initialise the Firebase Service.
+
+        Parameters
+        ----------
+        `emulator` : `bool`
+            `True` if the Firebase Emulator should be used
+        `service_key_path` : `str`
+            The path to the Firebase Service Key.
+        """
         if not Path(service_key_path).exists():
             print(f"{TColors.FAIL}ERROR - Service Key cannot be found{TColors.ENDC}")
             sys.exit(1)
@@ -20,8 +33,19 @@ class FirebaseService:
             load_dotenv()
 
     def upload(self, docs, collection: str):
-        """Upload the data to Firebase."""
+        """
+        Upload the data to Firebase.
+
+        Parameters
+        ----------
+        `docs`
+            The JSON objects to upload.
+        `collection` : `str`
+            The collection to upload the docs to.
+        """
         for doc in docs:
             self.db.collection(collection).add(doc)
 
-        print(f"{TColors.OKGREEN}{collection} data successfully uploaded to Firestore.{TColors.ENDC}")
+        print(
+            f"{TColors.OKGREEN}{collection} data successfully uploaded to Firestore.{TColors.ENDC}"
+        )
